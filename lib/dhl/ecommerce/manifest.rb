b513fd22e3decc4c918540090b1ddb774b6e5808
Francois Deschenes <fdeschenes@me.com>
2016-01-15 00:23:16 -0800

    Fixed an issue creating manifests.

--- lib/dhl/ecommerce/manifest.rb
+++ lib/dhl/ecommerce/manifest.rb
@@ -14,7 +14,7 @@ module DHL
 
       def self.create(labels)
         labels.group_by(&:location_id).each.collect do |location_id, location_labels|
-          closeout_id = DHL::Ecommerce.request :get, "https://api.dhlglobalmail.com/v1/{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/id"
+          closeout_id = DHL::Ecommerce.request :get, "https://api.dhlglobalmail.com/v1/#{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/id"
 
           location_labels.each_slice(500) do |slice_labels|
             xml = Builder::XmlMarkup.new
@@ -29,12 +29,12 @@ module DHL
               end
             end
 
-            DHL::Ecommerce.request :post, "https://api.dhlglobalmail.com/v1/{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/#{closeout_id}" do |request|
-              request.body xml.target!
+            DHL::Ecommerce.request :post, "https://api.dhlglobalmail.com/v1/#{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/#{closeout_id}" do |request|
+              request.body = xml.target!
             end
           end
 
-          response = DHL::Ecommerce.request :post, "https://api.dhlglobalmail.com/v1/{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/#{closeout_id}"
+          response = DHL::Ecommerce.request :get, "https://api.dhlglobalmail.com/v1/#{DHL::Ecommerce::Location.resource_name.downcase}s/#{location_id}/closeout/#{closeout_id}"
           response[:manifest_list][:manifest] = [response[:manifest_list][:manifest]] unless response[:manifest_list][:manifest].is_a? Array
           response[:manifest_list][:manifest].each.collect do |attributes|
             new attributes.merge(location_id: location_id)
